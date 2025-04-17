d2bcf57e2ba96ceaa4433aab46bd3cc7efe697f0
Prashantha K.G.M <it21169908@my.sliit.lk>
2025-04-17 10:56:38 +0530

    Course service - create routes

---
+++ services/course-management/src/routes/index.ts
@@ -0,0 +1,30 @@
+import {Router} from "express";
+import createHttpError from "http-errors";
+import {RabbitMQService} from "../services/RrabbitMQService";
+import CourseService from "../services/CourseService";
+import {CourseRoutesInit} from "./course";
+import {RPCObserver} from "../services/RPCService";
+import env from "../config";
+
+
+export async function initRoutes(router: Router) {
+
+    const rabbitMQService = await RabbitMQService.getInstance()
+    const courseService = new CourseService(rabbitMQService);
+
+    // TODO: Listen to the events
+    await rabbitMQService.subscribeMessage(courseService)
+    await RPCObserver(env.COURSE_RPC, courseService);
+
+    router.get('', (req, res) => {
+        res.json("Course Management service™ API").status(200);
+    });
+
+    router.get('/test', courseService.test);
+    CourseRoutesInit(router, courseService);
+
+    /* INVALID REQUESTS */
+    // app.get('/', (req: Request, res: Response) => res.redirect(301, "/api"));
+    router.use((req, res, next) => next(new createHttpError.NotFound()));
+    // app.all('*', (req: Request, res: Response) => res.send("Invalid Route").status(404));
+}
