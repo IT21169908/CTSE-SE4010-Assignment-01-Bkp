d05d3ca5c94e977ff8344b87c080a32eec018634
Fonseka M.M.N.H <it21156410@my.sliit.lk>
2025-04-17 12:00:09 +0530

    validation & error handling middlewares

--- services/auth/src/config/index.ts
+++ services/auth/src/config/index.ts
@@ -4,11 +4,16 @@ const env = {
     APP_SECRET: process.env.JWT_SECRET || "",
     EXCHANGE_NAME: process.env.EXCHANGE_NAME || "",
     MSG_QUEUE_URL: process.env.MSG_QUEUE_URL || "",
-
     QUEUE_NAME: "AUTH_QUEUE",
-
     AUTH_SERVICE: "auth_service",
+    COURSE_SERVICE: "course_service",
+    LMS_SERVICE: "lms_service",
+    NOTIFY_SERVICE: "notify_service",
 
+    COURSE_RPC: "COURSE_RPC",
     AUTH_RPC: "AUTH_RPC",
+    NOTIFY_RPC: "NOTIFY_RPC",
+    LMS_RPC: "LMS_RPC",
+    PAYMENT_RPC: "PAYMENT_RPC",
 };
 export default env
