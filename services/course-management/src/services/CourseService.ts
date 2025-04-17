ae162715db79aa43960a0d9e606804cb17b5f545
Prashantha K.G.M <it21169908@my.sliit.lk>
2025-04-17 11:03:04 +0530

    Course service - services

---
+++ services/course-management/src/services/CourseService.ts
@@ -0,0 +1,241 @@
+import {NextFunction, Request, Response} from "express";
+import {RabbitMQService} from "./RrabbitMQService";
+
+import env from "../config";
+import Course from "../repository/CourseRepository";
+import {CourseValidations} from "../validations/course-validations";
+import {validationsChecker} from "../validations/validation-handler";
+import {DCourse} from "../models/CourseModel";
+import {Types} from "mongoose";
+import {RPCRequest} from "./RPCService";
+
+const {AUTH_SERVICE, LMS_SERVICE} = env
+
+class CourseService {
+
+    private readonly rabbitMQ: RabbitMQService;
+    private readonly courseRepository: Course;
+
+    constructor(rabbitMQ: RabbitMQService) {
+        this.rabbitMQ = rabbitMQ;
+        this.courseRepository = new Course()
+        this.test = this.test.bind(this);
+    }
+
+    public test(req: Request, res: Response, next: NextFunction) {
+        const user = req.user
+
+        const authPayload = {
+            event: "LOGIN",
+            data: {
+                "_id": user?._id || null,
+                "name": user?.name || null,
+                "email": user?.email || null,
+                "role": user?.role || null,
+                "permissions": user?.permissions || null,
+                "signedUpAs": user?.signedUpAs || null,
+                "phone": user?.phone || null,
+                "lastLoggedIn": user?.lastLoggedIn || null,
+            }
+        }
+
+        const lmsPayload = {
+            event: "SAMPLE",
+            data: {
+                "_id": "6600535b01e929e77376118d",
+                "user_id": "66004de7d5dd17b8991741e5",
+                "course_id": "65ffef1b021037df10627bba",
+                "status": "approved",
+            }
+        }
+
+        // TODO: Publish service events
+        this.rabbitMQ.publishMessage(AUTH_SERVICE, JSON.stringify(authPayload))
+        this.rabbitMQ.publishMessage(LMS_SERVICE, JSON.stringify(lmsPayload))
+
+
+        res.sendSuccess({authPayload: authPayload, lmsPayload: lmsPayload,}, "COURSE SERVICE TEST ROUTE™ API");
+    }
+
+    public createCourseValidationRules() {
+        return [
+            CourseValidations.name(),
+            CourseValidations.code(),
+            CourseValidations.description(),
+            CourseValidations.credits(),
+            CourseValidations.fee(),
+            // CourseValidations.lecturerId(),
+        ];
+    }
+
+    public updateCourseValidationRules() {
+        return this.createCourseValidationRules();
+    }
+
+    public fetchCourseValidationRules() {
+        return [];
+    }
+
+// ================ CREATE - C ================
+    public create(req: Request, res: Response, next: NextFunction) {
+        if (validationsChecker(req, res)) {
+            const user = req.user;
+
+            const {name, code, fee, description, credits, status} = req.body;
+            const data: DCourse = {
+                status: status,
+                name: name,
+                code: code,
+                fee: fee,
+                description: description,
+                credits: credits,
+                lecture_id: user?._id!
+            };
+            this.courseRepository.createCourse(data, user).then(course => {
+                res.sendSuccess(course, "Course created successfully!");
+            }).catch(next);
+        }
+    }
+
+// ================ READ - R ================
+    public getAll(req: Request, res: Response, next: NextFunction) {
+        const user = req.user;
+        this.courseRepository.getAllCourses(user).then(courses => {
+            res.sendSuccess(courses, "Get all courses successfully!");
+        }).catch(next);
+    }
+
+    public getCoursesByLecturerId(req: Request, res: Response, next: NextFunction) {
+        const user = req.user;
+        const lecturerId = req.params._lecture as unknown as Types.ObjectId;
+        this.courseRepository.getCoursesByLecturerId(lecturerId).then(courses => {
+            res.sendSuccess(courses, "Get all courses successfully!");
+        }).catch(next);
+    }
+
+    public getMyCourses(req: Request, res: Response, next: NextFunction) {
+        const user = req.user;
+        this.courseRepository.getCoursesByLecturerId(user?._id!).then(courses => {
+            res.sendSuccess(courses, "Get all courses successfully!");
+        }).catch(next);
+    }
+
+    public getEnrolledUsers(req: Request, res: Response, next: NextFunction) {
+        const user = req.user;
+        this.courseRepository.getCoursesByLecturerId(user?._id!).then(async courses => {
+            const enrollments = await RPCRequest(env.LMS_RPC, {
+                event: "GET_ENROLLED_USERS", data: {
+                    course_ids: courses.map(c => c._id)
+                }
+            })
+            if (enrollments !== null && typeof enrollments === 'object') {
+                res.sendSuccess(enrollments, "Get all courses successfully!");
+                // const enrolled_students = await RPCRequest(env.AUTH_RPC, {
+                //     event: "GET_ENROLLED_USERS", data: {
+                //         course_ids: enrollments.map(c => c._id)
+                //     }
+                // })
+            }
+            res.sendSuccess([], "Enrollments doe not exists")
+            // res.sendSuccess(courses, "Get all courses successfully!");
+        }).catch(next);
+    }
+
+    public getById(req: Request, res: Response, next: NextFunction) {
+        if (validationsChecker(req, res)) {
+            const user = req.user;
+            const courseId = req.params._id as unknown as Types.ObjectId;
+            this.courseRepository.getCourseById(courseId, user).then(course => {
+                res.sendSuccess(course, "Get course by ID successfully!");
+            }).catch(next);
+        }
+    }
+
+// ================ UPDATE - U ================
+    public async update(req: Request, res: Response, next: NextFunction) {
+        if (validationsChecker(req, res)) {
+            const user = req.user;
+            const courseId = req.params._id as unknown as Types.ObjectId;
+            const {name, code, description, fee, credits, lecture_id} = req.body;
+            const courseDetails: Partial<DCourse> = {
+                name: name,
+                code: code,
+                fee: fee,
+                description: description,
+                credits: credits,
+                lecture_id: lecture_id,
+            };
+            this.courseRepository.updateCourse(courseId, courseDetails, user).then(course => {
+                res.sendSuccess(course, "Course updated successfully!");
+            }).catch(next);
+        }
+    }
+
+// ================ DELETE - D ================
+    public deleteCourse(req: Request, res: Response, next: NextFunction) {
+        if (validationsChecker(req, res)) {
+            const courseId = req.params._id as unknown as Types.ObjectId;
+            const user = req.user;
+            this.courseRepository.deleteCourseById(courseId, user).then(course => {
+                res.sendSuccess(course, "Course deleted successfully!");
+            }).catch(next);
+        }
+    }
+
+    async SubscribeEvents(payload: string): Promise<void> {
+        console.log('Triggering.... CoursesService Events');
+
+        const parsedPayload: PayloadData = JSON.parse(payload);
+
+        const {event, data} = parsedPayload;
+        const {id, name, price, isActive} = data;
+
+        switch (event) {
+            case 'SAMPLE':
+                this.sample(parsedPayload);
+                break;
+            default:
+                break;
+        }
+    }
+
+    async SubscribeRPCObserver(payload: string): Promise<object> {
+        console.log('Triggering.... CoursesService SubscribeRPCObserver');
+
+        const parsedPayload: PayloadData = JSON.parse(payload);
+
+        const {event, data} = parsedPayload;
+        const {id, name, price, isActive} = data;
+
+        switch (event) {
+            case 'SAMPLE':
+                return this.sample(parsedPayload);
+            default:
+                return {};
+        }
+    }
+
+    private sample(parsedPayload: PayloadData): object {
+        // Implement your logic here
+        console.log(`CourseService sample SubscribeEvents parsedPayload: `, parsedPayload);
+        return {
+            _id: "yt686tu8763tyyr98734",
+            name: "CourseService sample SubscribeRPCObserver",
+            credits: 5,
+            fee: 2,
+        }
+
+    }
+}
+
+interface PayloadData {
+    event: string;
+    data: {
+        id: string;
+        name: string;
+        price: string;
+        isActive: boolean;
+    };
+}
+
+export default CourseService
---
+++ services/course-management/src/services/RPCService.ts
@@ -0,0 +1,102 @@
+import * as amqplib from 'amqplib';
+import env from "../config";
+import CourseService from "./CourseService";
+import {v4 as uuid4} from "uuid";
+
+
+const {
+    MSG_QUEUE_URL,
+} = env
+
+let amqplibConnection: amqplib.Connection | null = null;
+
+export const getChannel = async () => {
+    if (amqplibConnection === null) {
+        amqplibConnection = await amqplib.connect(MSG_QUEUE_URL);
+    }
+    return await amqplibConnection.createChannel();
+};
+
+export const RPCObserver = async (RPC_QUEUE_NAME: string, service: CourseService) => {
+    const channel = await getChannel();
+    await channel.assertQueue(RPC_QUEUE_NAME, {
+        durable: false,
+    });
+    await channel.prefetch(1);
+    await channel.consume(
+        RPC_QUEUE_NAME,
+        async (msg) => {
+            if (msg !== null && msg.content) {
+                let response = {};
+                // DB Operation
+
+                response = await service.SubscribeRPCObserver(msg.content.toString()); // call DB operation
+
+                channel.sendToQueue(
+                    msg.properties.replyTo,
+                    Buffer.from(JSON.stringify(response)),
+                    {
+                        correlationId: msg.properties.correlationId,
+                    }
+                );
+                channel.ack(msg);
+            }
+        },
+        {
+            noAck: false,
+        }
+    );
+};
+
+export const requestData = async (RPC_QUEUE_NAME: string, requestPayload: {
+    event: string,
+    data: {}
+}, uuid: string) => {
+    try {
+        const channel = await getChannel();
+
+        const q = await channel.assertQueue("", {exclusive: true});
+
+        channel.sendToQueue(
+            RPC_QUEUE_NAME,
+            Buffer.from(JSON.stringify(requestPayload)),
+            {
+                replyTo: q.queue,
+                correlationId: uuid,
+            }
+        );
+
+        return new Promise((resolve, reject) => {
+            // timeout n
+            const timeout = setTimeout(() => {
+                channel.close();
+                resolve("API could not fullfil the request!");
+            }, 8000);
+            channel.consume(
+                q.queue,
+                (msg) => {
+                    if (msg !== null && msg.properties.correlationId == uuid) {
+                        resolve(JSON.parse(msg.content.toString()));
+                        clearTimeout(timeout);
+                    } else {
+                        reject("data Not found!");
+                    }
+                },
+                {
+                    noAck: true,
+                }
+            );
+        });
+    } catch (error) {
+        console.log(error);
+        return "error";
+    }
+};
+
+export const RPCRequest = async (RPC_QUEUE_NAME: string, requestPayload: {
+    event: string,
+    data: {}
+}) => {
+    const uuid = uuid4(); // correlationId
+    return await requestData(RPC_QUEUE_NAME, requestPayload, uuid);
+};
