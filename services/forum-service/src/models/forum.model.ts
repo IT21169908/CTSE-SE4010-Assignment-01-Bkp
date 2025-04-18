a3724a7dda93020492f8e8e4a930e4e64b63d6e9
Hasitha <it21175152@my.sliit.lk>
2025-04-18 22:51:17 +0530

    fix: update the forum model

--- services/forum-service/src/models/forum.model.ts
+++ services/forum-service/src/models/forum.model.ts
@@ -1,17 +1,34 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
+import Conversation from "./conversation.model";
 
 export interface IForum extends Document {
   title: string;
   content: string;
-  author: string;
-  createdAt: Date;
+  courseId: string;
+  createdBy: Types.ObjectId;
+  conversations: Types.ObjectId[];
 }
 
-const ForumSchema: Schema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+const forumSchema = new Schema<IForum>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    courseId: { type: String, required: true },
+    createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    conversations: [{ type: Schema.Types.ObjectId, ref: "Conversation" }],
+  },
+  { timestamps: true }
+);
+
+// Middleware to auto-delete associated conversations
+forumSchema.pre("findOneAndDelete", async function (next) {
+  const forum = await this.model.findOne(this.getFilter());
+
+  if (forum) {
+    await Conversation.deleteMany({ _id: { $in: forum.conversations } });
+  }
+
+  next();
 });
 
-export default mongoose.model<IForum>("Forum", ForumSchema);
+export default mongoose.model<IForum>("Forum", forumSchema);
