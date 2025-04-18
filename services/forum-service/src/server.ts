0d26b152ebecf1c58483bf8f52ba2868cba8d825
Hasitha <it21175152@my.sliit.lk>
2025-04-18 23:40:01 +0530

    fix: fix aggregation issue

--- services/forum-service/src/server.ts
+++ services/forum-service/src/server.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   await connectDB();
-  await connectToBroker();
+  // await connectToBroker();
 
   app.listen(PORT, () => {
     console.log(`Forum Service running at http://localhost:${PORT}`);
