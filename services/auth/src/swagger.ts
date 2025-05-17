086ea0bddd1d230ab8e5453a8f4b65f97a5c255e
Fonseka M.M.N.H <it21156410@my.sliit.lk>
2025-05-17 18:42:59 +0530

    change auth service swagger url

--- services/auth/src/swagger.ts
+++ services/auth/src/swagger.ts
@@ -12,7 +12,7 @@
 
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-import { Express } from "express";
+import {Express} from "express";
 import fs from 'fs';
 import path from 'path';
 
@@ -20,29 +20,32 @@ import path from 'path';
 let existingSwagger = {};
 const swaggerJsonPath = path.join(__dirname, '../swagger.json');
 if (fs.existsSync(swaggerJsonPath)) {
-  try {
-    existingSwagger = JSON.parse(fs.readFileSync(swaggerJsonPath, 'utf8'));
-  } catch (error) {
-    console.error('Error reading swagger.json:', error);
-  }
+    try {
+        existingSwagger = JSON.parse(fs.readFileSync(swaggerJsonPath, 'utf8'));
+    } catch (error) {
+        console.error('Error reading swagger.json:', error);
+    }
 }
 
 const options: swaggerJSDoc.Options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Auth Microservice API",
-      version: "1.0.0",
-      description: "API documentation for the Auth microservice.",
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Auth Microservice API",
+            version: "1.0.0",
+            description: "API documentation for the Auth microservice.",
+        },
+        servers: [
+            {
+                url: "http://localhost:8001",
+            },
+            {
+                url: "http://k8s-default-authserv-2eab4279c2-3e2e1a8573b10dd1.elb.ap-south-1.amazonaws.com:8001",
+            },
+        ],
+        ...existingSwagger, // Merge with existing swagger.json
     },
-    servers: [
-      {
-        url: "http://localhost:8001",
-      },
-    ],
-    ...existingSwagger, // Merge with existing swagger.json
-  },
-  apis: ["./src/*.ts", "./src/routes/*.ts", "./src/models/*.ts", "./src/schemas/*.ts"], // path to files with annotations
+    apis: ["./src/*.ts", "./src/routes/*.ts", "./src/models/*.ts", "./src/schemas/*.ts"], // path to files with annotations
 };
 
 const swaggerSpec = swaggerJSDoc(options);
@@ -51,11 +54,11 @@ const swaggerSpec = swaggerJSDoc(options);
 fs.writeFileSync(swaggerJsonPath, JSON.stringify(swaggerSpec, null, 2));
 
 export const setupSwagger = (app: Express) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-  // Endpoint to get the Swagger JSON
-  app.get('/swagger.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
-  });
+    // Endpoint to get the Swagger JSON
+    app.get('/swagger.json', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec);
+    });
 };
